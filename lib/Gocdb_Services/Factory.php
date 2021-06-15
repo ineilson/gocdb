8df09e833fbaafa87042107b516fc40f9911de8e
Ian Neilson <ian.neilson@stfc.ac.uk>
2021-06-15 11:26:29 +0000

    Authorisation based on user role for the API
    Update Site service to use new APIAuthentication service

--- lib/Gocdb_Services/Factory.php
+++ lib/Gocdb_Services/Factory.php
@@ -48,6 +48,7 @@ class Factory {
     private static $OwnedEntityService = null;
     private static $exService = null;
     private static $notificationService = null;
+    private static $APIAuthenticationService = null;
 
     public static $properties = array();
     //private static $properties = null;
@@ -384,6 +385,19 @@ class Factory {
             self::$notificationService->setEntityManager(self::getEntityManager());
         }
         return self::$notificationService;
+    }
+
+    /**
+     * Singleton APIAuthenticationService service
+     * @return org\gocdb\services\APIAuthenticationService
+     */
+    public static function getAPIAuthenticationService() {
+        if (self::$APIAuthenticationService == null) {
+            require_once __DIR__ . '/APIAuthenticationService.php';
+            self::$APIAuthenticationService = new org\gocdb\services\APIAuthenticationService();
+            self::$APIAuthenticationService->setEntityManager(self::getEntityManager());
+        }
+        return self::$APIAuthenticationService;
     }
 }
 
